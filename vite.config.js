3d3127d9a4102a0ae04d3224d36c03195f5df1dc
Kevin <99073020@mydavinci.nl>
2025-05-02 15:44:42 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/kevin-portfolio',            
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
+  base: '/kevin-portfolio/',            
 })
